722a02a39b23d9ed7dce8e22666867f5645983a8
ArunManoj005 <arunmanoj005@gmail.com>
2024-04-11 19:34:58 +0530

    home page added

--- src/config.js
+++ src/config.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose');
-const connect = mongoose.connect("mongodb://localhost:27017/mydb");
 
 // Check database connected or not
-connect.then(() => {
-    console.log("Database Connected Successfully");
-})
-.catch(() => {
-    console.log("Database cannot be Connected");
-})
+
 
 // Create Schema
 const Loginschema = new mongoose.Schema({
@@ -22,7 +16,11 @@ const Loginschema = new mongoose.Schema({
     password: {
         type:String,
         required: true
-    }
+    },
+    user_type: {
+        type:String,
+        required: true
+    },
 });
 
 // collection part
